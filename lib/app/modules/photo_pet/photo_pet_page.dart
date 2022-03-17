3366dd94179f319cb2f4436e40df95fd4f541d33
Renato Alves <renato@Mac-mini-de-Renato.local>
2022-03-16 23:31:13 -0300

    trabalhando na tela de imagem do pet.

--- lib/app/modules/photo_pet/photo_pet_page.dart
+++ lib/app/modules/photo_pet/photo_pet_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:progress_stepper/progress_stepper.dart';
 import '../../components/instapet_appbar.dart';
 import '../../components/instapet_buttom.dart';
 import '../../core/core.dart';
@@ -56,25 +57,35 @@ class _PhotoPetPageState extends InstaState<PhotoPetPage, PhotoPetController> {
                   ),
                   textAlign: TextAlign.center,
                 ),
-                SizedBox(height: _responsive.dp(3.8)),
+                SizedBox(height: _responsive.dp(2.6)),
                 image != null
                     ? ClipRRect(
-                        borderRadius: BorderRadius.circular(300),
+                        borderRadius: BorderRadius.circular(600),
                         child: Image.file(
                           image!,
-                          width: 200,
-                          height: 200,
+                          fit: BoxFit.cover,
+                          height: _responsive.dp(36),
+                          width: _responsive.dp(36),
                         ),
                       )
                     : ClipRRect(
-                        borderRadius: BorderRadius.circular(300),
-                        child: Image.asset(
-                          'assets/images/cropped-akps_header.png',
-                          width: 200,
-                          height: 200,
+                        borderRadius: BorderRadius.circular(600),
+                        child: SizedBox(
+                          child: Icon(
+                            Icons.question_mark,
+                            size: _responsive.dp(26),
+                          ),
+                          height: _responsive.dp(36),
+                          width: _responsive.dp(36),
                         ),
+                        // child: Image.asset(
+                        //   'assets/images/cropped-akps_header.png',
+                        //   fit: BoxFit.cover,
+                        //   height: _responsive.dp(36),
+                        //   width: _responsive.dp(36),
+                        // ),
                       ),
-                SizedBox(height: _responsive.dp(10)),
+                SizedBox(height: _responsive.dp(2.6)),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
@@ -109,105 +120,43 @@ class _PhotoPetPageState extends InstaState<PhotoPetPage, PhotoPetController> {
                   label: 'CADASTRAR',
                   onPressed: () {},
                   width: context.width,
-                )
-                // buidButton(
-                //   title: 'Galeria',
-                //   icon: Icons.file_copy_outlined,
-                //   onClicked: () => pickImage(ImageSource.gallery),
-                // ),
-                // SizedBox(height: _responsive.dp(3.8)),
-                // buidButton(
-                //   title: 'Camera',
-                //   icon: Icons.camera_alt_outlined,
-                //   onClicked: () => pickImage(ImageSource.camera),
-                // ),
+                ),
+                SizedBox(height: _responsive.dp(2)),
+                ProgressStepper(
+                  width: 200,
+                  height: 25,
+                  color: Colors.red,
+                  progressColor: Colors.green,
+                  stepCount: 3,
+                  builder: (index) {
+                    double widthOfStep = 200 / 3;
+                    if (index == 1) {
+                      return ProgressStepWithArrow(
+                        width: widthOfStep,
+                        defaultColor: Colors.red,
+                        progressColor: Colors.green,
+                        wasCompleted: true,
+                        child: SizedBox(
+                          child: Text('Teste'),
+                          height: 30,
+                        ),
+                      );
+                    }
+                    return ProgressStepWithChevron(
+                      width: widthOfStep,
+                      defaultColor: Colors.red,
+                      progressColor: Colors.green,
+                      wasCompleted: false,
+                    );
+                  },
+                ),
+                SizedBox(height: _responsive.dp(2)),
               ],
             ),
           ),
         ),
       ),
-      // body: Container(
-      //   padding: EdgeInsets.all(32),
-      //   child: Column(
-      //     crossAxisAlignment: CrossAxisAlignment.center,
-      //     children: [
-      //       // SizedBox(height: _responsive.dp(2)),
-      //       Spacer(),
-      //       image != null
-      //           ? ClipOval(
-      //               child: Image.file(
-      //                 image!,
-      //                 width: 100,
-      //                 height: 120,
-      //               ),
-      //             )
-      //           : ClipOval(
-      //               child: SizedBox(
-      //                 width: 160,
-      //                 height: 160,
-      //                 child:
-      //                     Image.asset('assets/images/cropped-akps_header.png'),
-      //                 // child: Icon(
-      //                 //   Icons.image,
-      //                 //   size: 60,
-      //                 // ),
-      //               ),
-      //             ),
-      //       // FlutterLogo(size: 160),
-      //       SizedBox(height: _responsive.dp(2)),
-      //       Center(
-      //         child: Text(
-      //           'Insira uma foto de seu amiguinho.',
-      //           style: context.textTheme.headline6?.copyWith(
-      //             fontWeight: FontWeight.bold,
-      //             color: context.theme.primaryColorDark,
-      //           ),
-      //           textAlign: TextAlign.center,
-      //         ),
-      //       ),
-      //       SizedBox(height: _responsive.dp(3.8)),
-      //       buidButton(
-      //         title: 'Galeria',
-      //         icon: Icons.camera_alt_outlined,
-      //         onClicked: () => pickImage(ImageSource.gallery),
-      //       ),
-      //       SizedBox(height: _responsive.dp(3.8)),
-      //       buidButton(
-      //         title: 'Camera',
-      //         icon: Icons.camera_alt_outlined,
-      //         onClicked: () => pickImage(ImageSource.camera),
-      //       ),
-      //       Spacer(),
-      //     ],
-      //   ),
-      // ),
     );
   }
 }
-
 //https://www.youtube.com/watch?v=MSv38jO4EJk&t=82s
-
-Widget buidButton({
-  required String title,
-  required IconData icon,
-  required VoidCallback onClicked,
-}) =>
-    ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        minimumSize: Size.fromHeight(56),
-        // primary: Colors.white,
-        // onPrimary: Colors.black,
-        textStyle: TextStyle(fontSize: 20),
-      ),
-      onPressed: onClicked,
-      child: Row(
-        children: [
-          Icon(
-            icon,
-            size: 28,
-            color: Colors.black,
-          ),
-          const SizedBox(width: 16),
-        ],
-      ),
-    );

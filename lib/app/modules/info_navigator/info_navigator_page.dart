be297a11c20d0e8be7ae8b5a7dbe14032cbd3f9e
Renato Alves <renato@Mac-mini-de-Renato.local>
2022-03-24 22:09:41 -0300

    Trabalhando na tela de dados do Pet.

--- lib/app/modules/info_navigator/info_navigator_page.dart
+++ lib/app/modules/info_navigator/info_navigator_page.dart
@@ -56,21 +56,156 @@ class _InfoNavigatorPageState
                   ),
           ),
           Positioned(
-            top: _responsive.dp(36),
+            top: _responsive.dp(32),
             right: _responsive.dp(5),
             left: _responsive.dp(5),
             child: Container(
               padding: EdgeInsets.all(_responsive.dp(1)),
               decoration: BoxDecoration(
                 border: Border.all(
-                  width: 5,
-                  color: Colors.grey, // red as border color
+                  width: 6,
+                  color: PaletaCores.scaffold,
                 ),
               ),
-              child:
-                  Text("Sou um cãozinho tímido, mas que conquista corações!"),
+              child: Text(
+                "Sou um cãozinho tímido, mas que conquista corações!",
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-          )
+          ),
+          Positioned(
+            top: _responsive.dp(43),
+            right: _responsive.dp(5),
+            left: _responsive.dp(5),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Container(
+                //   height: _responsive.dp(6),
+                //   width: _responsive.dp(26),
+                //   decoration: BoxDecoration(
+                //     border: Border.all(
+                //       color: Colors.grey.shade300,
+                //     ),
+                //     borderRadius: BorderRadius.circular(10.0),
+                //   ),
+                //   child: Center(
+                //     child: Text(
+                //       'Beagle',
+                //       style: TextStyle(
+                //         fontSize: _responsive.dp(2.8),
+                //       ),
+                //     ),
+                //   ),
+                // ),
+                // SizedBox(height: _responsive.dp(1)),
+                // Container(
+                //   height: _responsive.dp(6),
+                //   width: _responsive.dp(26),
+                //   decoration: BoxDecoration(
+                //     border: Border.all(
+                //       color: Colors.grey.shade300,
+                //     ),
+                //     borderRadius: BorderRadius.circular(10.0),
+                //   ),
+                //   child: Center(
+                //     child: Text(
+                //       'Masculino',
+                //       style: TextStyle(
+                //         fontSize: _responsive.dp(2.8),
+                //       ),
+                //     ),
+                //   ),
+                // ),
+                // ClipRRect(
+                //   borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                //   child: Container(
+                //     height: _responsive.dp(7),
+                //     width: _responsive.dp(30),
+                //     padding: const EdgeInsets.all(16.0),
+                //     decoration: BoxDecoration(
+                //       border: Border(
+                //         left: BorderSide(
+                //           width: 8.0,
+                //           color: PaletaCores.principal,
+                //         ),
+                //         right: BorderSide(
+                //           width: 8.0,
+                //           color: PaletaCores.principal,
+                //         ),
+                //       ),
+                //     ),
+                //     child: Center(child: Text('Beagle')),
+                //   ),
+                // ),
+                // ClipRRect(
+                //   borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                //   child: Container(
+                //     height: _responsive.dp(7),
+                //     width: _responsive.dp(30),
+                //     padding: const EdgeInsets.all(16.0),
+                //     decoration: BoxDecoration(
+                //       border: Border(
+                //         left: BorderSide(
+                //           width: 8.0,
+                //           color: PaletaCores.principal,
+                //         ),
+                //         right: BorderSide(
+                //           width: 8.0,
+                //           color: PaletaCores.principal,
+                //         ),
+                //       ),
+                //     ),
+                //     child: Center(child: Text('Masculino')),
+                //   ),
+                // ),
+                Container(
+                  child: const Center(
+                    child: Text(
+                      'Beagle',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(color: PaletaCores.principal, spreadRadius: 8),
+                      BoxShadow(color: PaletaCores.scaffold, spreadRadius: 10),
+                    ],
+                  ),
+                  height: _responsive.dp(6),
+                  width: _responsive.dp(28),
+                ),
+                SizedBox(height: 8),
+                Container(
+                  child: const Center(
+                    child: Text(
+                      'Masculino',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(color: PaletaCores.principal, spreadRadius: 8),
+                      BoxShadow(color: PaletaCores.scaffold, spreadRadius: 10),
+                    ],
+                  ),
+                  height: _responsive.dp(6),
+                  width: _responsive.dp(28),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
       // builder: (_) => Column(
